7f1bafa11f37f2bde8874287251472182c5bab23
Dominique Marcadet <Dominique.Marcadet@centralesupelec.fr>
2019-07-02 15:38:54 +0200

    add new CimResourceSet and CimResourceSetFactory

---
+++ fr.centralesupelec.edf.riseclipse.cim.cim17.util/src/fr/centralesupelec/edf/riseclipse/cim/cim17/util/CimResourceSetImpl.java
@@ -0,0 +1,28 @@
+/**
+ *  Copyright (c) 2019 CentraleSupélec & EDF.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *  This file is part of the RiseClipse tool
+ *  
+ *  Contributors:
+ *      Computer Science Department, CentraleSupélec
+ *      EDF R&D
+ *  Contacts:
+ *      dominique.marcadet@centralesupelec.fr
+ *      aurelie.dehouck-neveu@edf.fr
+ *  Web site:
+ *      http://wdi.supelec.fr/software/RiseClipse/
+ * 
+ */
+package fr.centralesupelec.edf.riseclipse.cim.cim17.util;
+
+
+public abstract class CimResourceSetImpl extends fr.centralesupelec.edf.riseclipse.cim.util.cimxml.CimResourceSetImpl {
+
+    public CimResourceSetImpl( boolean strictContent ) {
+        super( strictContent );
+    }
+}
